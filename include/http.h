9179244c67ceae079588830e2e5b2cedf61ea175
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2017-03-20 19:19:04 +0300

    std::unique_ptr<HttpParser> HttpParser::create()

--- include/http.h
+++ include/http.h
@@ -1,19 +1,81 @@
 #pragma once
 
 #include <memory>
+#include <functional>
 #include <string>
+
+extern "C" {
+    #include <http_parser.h>
+}
 
 class HttpParser
 {
 public:
+    HttpParser() = delete;
+    HttpParser(const HttpParser&) = delete;
+    HttpParser& operator= (const HttpParser&) = delete;
+
+    struct OnHeadersComplete_Args;
+    using OnHeadersComplete = std::function<bool(const OnHeadersComplete_Args&)>;
+    using OnBody = std::function<void(const char*, std::size_t)>;
+    using OnComplete = std::function<void()>;
+
+public:
+    template<typename T1, typename T2, typename T3>
+    static std::unique_ptr<HttpParser> create(T1&& on_headers_complete, T2&& on_body, T3&& on_complete)
+    {
+        return std::unique_ptr<HttpParser>{ new HttpParser(
+                        std::forward<T1>(on_headers_complete),
+                        std::forward<T2>(on_body),
+                        std::forward<T3>(on_complete) )
+        };
+    }
+
+    class Error
+    {
+    public:
+        explicit Error(enum http_errno code) noexcept
+            : m_code{code}
+        {}
+
+        explicit operator bool () const noexcept
+        {
+             return m_code != HPE_OK;
+        }
+
+        enum http_errno code() const noexcept
+        {
+            return m_code;
+        }
+
+        const char* str() const noexcept
+        {
+            return http_errno_description(m_code);
+        }
+
+    private:
+        enum http_errno m_code;
+    };
+
     struct UriParseResult;
     static std::shared_ptr<UriParseResult> uri_parse(const std::string&);
 
-    struct ResponseParseResult;
-    static std::shared_ptr<ResponseParseResult> respone_parse(const std::string&);
+private:
+    template<typename T1, typename T2, typename T3>
+    HttpParser(T1&&, T2&&, T3&&)
+    {
 
-    class BodyDecoder;
-    class BodyDecoderContentLength;
+    }
+
+    http_parser parser;
+    http_parser_settings settings;
+    struct http_parser_data
+    {
+        //bool continue_after_headers = false;
+        //OnHeadersComplete on_headers_complete;
+        //OnBody
+    } data;
+
 };
 
 struct HttpParser::UriParseResult
@@ -24,52 +86,4 @@ struct HttpParser::UriParseResult
     std::string query;
     std::string username;
     std::string password;
-};
-
-struct HttpParser::ResponseParseResult
-{
-    bool is_done;
-    std::size_t http_code;
-    std::string http_reason;
-    const char* body;
-    std::size_t body_length;
-    std::string redirect_uri;
-    std::unique_ptr<BodyDecoder> body_decoder;
-};
-
-struct DataChunk
-{
-    std::shared_ptr<char> buffer;
-    std::size_t length;
-};
-
-class HttpParser::BodyDecoder
-{
-public:
-
-    struct Result
-    {
-        DataChunk data;
-        bool is_done;
-    };
-
-    virtual Result operator() (const char*, std::size_t) = 0;
-    virtual ~BodyDecoder() = default;
-};
-
-class HttpParser::BodyDecoderContentLength : public BodyDecoder
-{
-public:
-    BodyDecoderContentLength() = delete;
-    BodyDecoderContentLength(const BodyDecoderContentLength&) = delete;
-    BodyDecoderContentLength& operator= (const BodyDecoderContentLength&) = delete;
-
-    BodyDecoderContentLength(std::size_t len)
-        : left_length{len}
-    {}
-
-    virtual Result operator() (const char*, std::size_t) override final;
-
-private:
-    std::size_t left_length;
 };
--- test/test_http_parser_response.cpp
+++ test/test_http_parser_response.cpp
@@ -7,16 +7,19 @@ extern "C" {
 #include <http_parser.h> // remove ???
 }
 
+using ::testing::_;
+using ::testing::SaveArg;
+
 using namespace std;
 
+/*
 TEST(http_parser, incomplete_body)
 {
     const string buf ="\
-HTTP/1.1 200 OK\r\n\
+HTTP/1.0 200 OK\r\n\
 Server: nginx/1.6.2\r\n\
 Date: Fri, 10 Mar 2017 18:11:04 GMT\r\n\
 Content-Type: text/pain\r\n\
-Content-Length: 12\r\n\
 Last-Modified: Fri, 10 Mar 2017 18:09:54 GMT\r\n\
 Connection: keep-alive\r\n\
 ETag: \"58c2fb69-c\"\r\n\
@@ -25,8 +28,7 @@ Accept-Ranges: bytes\r\n\
 Hello world\
 ";
 
-//    std::cout << "buf.size() => " << buf.size() << std::endl;
-
+//
     http_parser parser;
     http_parser_init(&parser, HTTP_RESPONSE);
     struct http_parser_data
@@ -63,66 +65,162 @@ Hello world\
     {
         auto data_ptr = reinterpret_cast<http_parser_data*>(parser->data);
         data_ptr->body_length = len;
+        cout << "on_body! len => " << len << endl;
         return 0;
     };
     settings.on_body = on_boby;
 
-    const size_t nparsed = http_parser_execute(&parser, &settings, buf.data(), buf.size());
+    auto on_message_complete = [](http_parser*) -> int
+    {
+        cout << "on_message_complete!" << endl;
+        return 0;
+    };
+    settings.on_message_complete = on_message_complete;
 
-//    const char* str = http_errno_name( static_cast<enum http_errno>(parser.http_errno) );
-//    std::cout << str << std::endl;
+    const size_t nparsed = http_parser_execute(&parser, &settings, buf.data(), buf.size());
+    cout << "send next data in parser... " << endl;
+    http_parser_execute(&parser, &settings, "next_data", 5);
+    cout << "send EOF in parser... " << endl;
+    http_parser_execute(&parser, &settings, nullptr, 0);
+
+    const char* str = "11"
+            "22";
+    std::cout << str << std::endl;
 
     cout << "http_status => " << data.http_status << endl;
 
     ASSERT_EQ(nparsed, buf.size());
     ASSERT_EQ(parser.status_code, 200u);
 
-    ASSERT_EQ(data.content_length, 12u);
+    //ASSERT_EQ(data.content_length, 12u);
     ASSERT_EQ(data.body_length, 11u);
     ASSERT_EQ(data.http_code, 200u);
 }
+*/
 
-TEST(HttpParser__BodyDecoderContentLength, normal)
+TEST(http_parser, on_headers_complete_return_1)
 {
-    const string s1 = "QWERTYUIOP";
-    const string s2 = "ASDFGHJKL";
+    const string buf = ""
+            "HTTP/1.1 301 Moved Permanently\r\n"
+            "Server: nginx/1.6.2\r\n"
+            "Date: Fri, 10 Mar 2017 18:11:04 GMT\r\n"
+            "Content-Type: text/pain\r\n"
+            "Contenet-Length: 12\r\n"
+            "Last-Modified: Fri, 10 Mar 2017 18:09:54 GMT\r\n"
+            "Location: http://www.example.org/index.asp\r\n"
+            "Connection: keep-alive\r\n"
+            "ETag: \"58c2fb69-c\"\r\n"
+            "Accept-Ranges: bytes\r\n"
+            "\r\n"
+            "Hello world!";
+//
+    http_parser parser;
+    http_parser_init(&parser, HTTP_RESPONSE);
+    struct http_parser_data
+    {
+        bool on_headers_complete = false;
+        bool on_body = false;
+        bool on_message_complete = false;
+        size_t nread = 0;
+    };
+    http_parser_data data;
+    parser.data = &data;
 
-    HttpParser::BodyDecoderContentLength body_decoder{ s1.size() + s2.size() };
+    http_parser_settings settings;
+    http_parser_settings_init(&settings);
+    settings.on_headers_complete = [](http_parser* parser_ptr) -> int
+    {
+        auto data_ptr = reinterpret_cast<http_parser_data*>(parser_ptr->data);
+        data_ptr->on_headers_complete = true;
+        data_ptr->nread = parser_ptr->nread;
+        return 0;
+    };
+    settings.on_body = [](http_parser* parser_ptr, const char* at, size_t len) -> int
+    {
+        auto data_ptr = reinterpret_cast<http_parser_data*>(parser_ptr->data);
+        data_ptr->on_body = true;
+        return 0;
+    };
+    settings.on_message_complete = [](http_parser* parser_ptr) -> int
+    {
+        auto data_ptr = reinterpret_cast<http_parser_data*>(parser_ptr->data);
+        data_ptr->on_message_complete = true;
+        return 0;
+    };
 
-    auto res1 = body_decoder( s1.data(), s1.length() );
-    ASSERT_FALSE(res1.is_done);
-    ASSERT_TRUE(res1.data.buffer);
-    const string str1{ res1.data.buffer.get(), res1.data.length };
-    ASSERT_EQ(str1, s1);
+    const size_t nparsed = http_parser_execute(&parser, &settings, buf.data(), buf.size());
+    cout << "http_errno => " << http_errno_description( static_cast<http_errno>(parser.http_errno) ) << endl;
+    cout << "parser.nread (on_headers_complete) => " << "\r" << data.nread << " left buf => " << buf.substr(data.nread) << endl;
 
-    auto res2 = body_decoder( s2.data(), s2.length() );
-    ASSERT_TRUE(res2.is_done);
-    ASSERT_TRUE(res2.data.buffer);
-    const string str2{ res2.data.buffer.get(), res2.data.length };
-    ASSERT_EQ(str2, s2);
+    ASSERT_TRUE(data.on_headers_complete);
+    //ASSERT_FALSE(data.on_body);
+    //ASSERT_TRUE(data.on_message_complete);
+
+    ASSERT_EQ(parser.http_errno, HPE_OK);
+
+    ASSERT_EQ(parser.status_code, 301u);
+    //ASSERT_EQ(nparsed, buf.size());
 }
 
-TEST(HttpParser__BodyDecoderContentLength, input_data_larger_content_length)
+TEST(HttpParser__Response, normal)
 {
-    const string s1 = "QWERTYUIOP";
-    const string s2 = "ASDFGHJKL";
-    const string s2_ = s2 + "ZXCVBNM";
+    HttpParser::OnHeadersComplete on_headers_complete;
+    HttpParser::OnBody on_body;
+    HttpParser::OnComplete on_complete;
 
-    HttpParser::BodyDecoderContentLength body_decoder{ s1.size() + s2.size() };
+    auto parser = HttpParser::create(on_headers_complete, on_body, on_complete);
+    ASSERT_TRUE(parser);
+}
 
-    auto res1 = body_decoder( s1.data(), s1.length() );
-    ASSERT_FALSE(res1.is_done);
-    ASSERT_TRUE(res1.data.buffer);
-    const string str1{ res1.data.buffer.get(), res1.data.length };
-    ASSERT_EQ(str1, s1);
+TEST(HttpParser__Response, rvalue_callbacks)
+{
+    HttpParser::OnHeadersComplete on_headers_complete;
+    HttpParser::OnBody on_body;
+    HttpParser::OnComplete on_complete;
 
-    auto res2 = body_decoder( s2_.data(), s2_.length() );
-    ASSERT_TRUE(res2.is_done);
-    ASSERT_TRUE(res2.data.buffer);
-    const string str2{ res2.data.buffer.get(), res2.data.length };
-    ASSERT_EQ(str2, s2);
+    auto parser = HttpParser::create(
+                std::move(on_headers_complete),
+                std::move(on_body),
+                std::move(on_complete)
+    );
+    ASSERT_TRUE(parser);
+}
 
-    auto res3 = body_decoder( s2_.data(), s2_.length() );
-    ASSERT_TRUE(res3.is_done);
-    ASSERT_FALSE(res3.data.buffer);
+
+class HttpParserMock
+{
+public:
+    static shared_ptr<HttpParserMock> instance;
+    static shared_ptr<HttpParserMock> create( function<void()> f)
+    {
+        instance->create_(f);
+        return instance;
+    }
+
+    MOCK_METHOD1(create_, void(function<void()>));
+};
+
+shared_ptr<HttpParserMock> HttpParserMock::instance;
+
+TEST(HttpParserMock, create)
+{
+    auto parser_ptr = make_shared<HttpParserMock>();
+    HttpParserMock::instance = parser_ptr;
+
+    function<void()> handler;
+    EXPECT_CALL(*parser_ptr, create_(_))
+            .WillOnce( SaveArg<0>(&handler) );
+
+    size_t i = 0;
+    auto cb = [&i]()
+    {
+        i++;
+    };
+
+    auto result_instance = HttpParserMock::create( std::move(cb) );
+
+    ASSERT_EQ(result_instance, parser_ptr);
+
+    handler();
+    ASSERT_EQ(i, 1u);
 }
