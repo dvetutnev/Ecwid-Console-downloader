6d71a4fca58dafc80fc95a44bee6476aecd97e02
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2017-04-24 23:04:27 +0300

    New HttpParser interface

--- include/http.h
+++ include/http.h
@@ -14,90 +14,37 @@ extern "C" {
 class HttpParser
 {
 public:
-    struct OnHeadersComplete_Args
-    {
-        std::size_t http_code = 0;
-        std::string http_reason;
-        std::size_t content_length = 0;
-        std::map< std::string, std::string > headers;
-    };
-    using OnHeadersComplete = std::function<bool( std::unique_ptr<OnHeadersComplete_Args> )>;
-    using OnBody = std::function<void( const char*, std::size_t )>;
-    using OnComplete = std::function<void()>;
+    using OnData = std::function<bool( std::unique_ptr<char>, unsigned int )>;
 
-    template< typename T1, typename T2, typename T3 >
+    template< typename T >
     static
-    typename std::enable_if_t<
-        std::is_convertible<T1, OnHeadersComplete>::value &&
-        std::is_convertible<T2, OnBody>::value &&
-        std::is_convertible<T3, OnComplete>::value,
-    std::unique_ptr<HttpParser> >
-    create( T1&& on_headers_complete, T2&& on_body, T3&& on_complete )
+    typename std::enable_if_t< std::is_convertible<T, OnData>::value, std::unique_ptr<HttpParser> >
+    create(T&& on_data)
     {
-        if (on_headers_complete == nullptr)
-            throw std::invalid_argument{"on_headers_complete should not be empty!"};
-
-        return std::unique_ptr<HttpParser>{ new HttpParser(
-                        std::forward<T1>(on_headers_complete),
-                        std::forward<T2>(on_body),
-                        std::forward<T3>(on_complete) )
-        };
+        return std::unique_ptr<HttpParser>{ new HttpParser( std::forward<T>(on_data) ) };
     }
 
-    class Error
+    struct ResponseParseResult
     {
-    public:
-        explicit Error(enum http_errno = HPE_OK) noexcept;
-        Error& operator= (const Error&) noexcept;
-        explicit operator bool () const noexcept;
-        enum http_errno code() const noexcept;
-        const char* str() const noexcept;
-    private:
-        enum http_errno m_code;
+        enum class State { InProgress, Done, Redirect, Error };
+        State state;
+        std::string redirect_uri;
+        std::string err_str;
     };
 
-    const Error response_parse(const char*, std::size_t);
+    ResponseParseResult response_parse(std::unique_ptr<char[]>, unsigned int);
 
     struct UriParseResult;
     static std::unique_ptr<UriParseResult> uri_parse(const std::string&);
 
 private:
-    template< typename T1, typename T2, typename T3 >
-    HttpParser(T1&& on_headers_complete, T2&& on_body, T3&& on_complete )
-        : cb_on_headers_complete{ std::forward<T1>(on_headers_complete) },
-          cb_on_body{ std::forward<T2>(on_body) },
-          cb_on_complete{ std::forward<T3>(on_complete) }
-    {
-        http_parser_init(&parser, HTTP_RESPONSE);
-        parser.data = this;
 
-        http_parser_settings_init(&settings);
-        settings.on_status = &HttpParser::on_status;
-        settings.on_header_field = &HttpParser::on_header_field;
-        settings.on_header_value = &HttpParser::on_header_value;
-        settings.on_headers_complete = &HttpParser::on_headers_complete;
-        settings.on_body = &HttpParser::on_body;
-        settings.on_message_complete = &HttpParser::on_complete;
-    }
+    template < typename T>
+    HttpParser(T&& on_data)
+        : cb_on_data{ std::forward<T>(on_data) }
+    {}
 
-    OnHeadersComplete cb_on_headers_complete;
-    OnBody cb_on_body;
-    OnComplete cb_on_complete;
-
-    http_parser parser;
-    http_parser_settings settings;
-
-    std::unique_ptr<OnHeadersComplete_Args> headers_complete_args;
-    std::string field_header, value_header;
-    enum class ModeHeader { Filed, Value };
-    ModeHeader mode_header = ModeHeader::Filed;
-
-    static int on_status(http_parser*, const char*, std::size_t);
-    static int on_header_field(http_parser*, const char*, std::size_t);
-    static int on_header_value(http_parser*, const char*, std::size_t);
-    static int on_headers_complete(http_parser*);
-    static int on_body(http_parser*, const char*, std::size_t);
-    static int on_complete(http_parser*);
+    OnData cb_on_data;
 
 public:
     HttpParser() = delete;
