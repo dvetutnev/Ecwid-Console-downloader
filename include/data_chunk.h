e98728636bdb43edd2a5ef6ab9ccc6646d6e378d
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2017-06-09 14:43:35 +0300

    DataChunk remove unused method

--- include/data_chunk.h
+++ include/data_chunk.h
@@ -4,21 +4,18 @@
 
 struct DataChunk
 {
-    DataChunk(std::unique_ptr<char[]> data_ = nullptr, std::size_t length_ = 0, std::size_t offset_ = 0) noexcept
+    DataChunk(std::unique_ptr<char[]> data_, std::size_t length_, std::size_t offset_ = 0) noexcept
         : data{ std::move(data_) },
           length{length_},
           offset{offset_}
     {}
 
-    DataChunk(DataChunk&& other) noexcept = default;
-    DataChunk& operator= (DataChunk&& other) noexcept = default;
+    DataChunk() = delete;
     DataChunk(const DataChunk&) = delete;
     DataChunk& operator= (const DataChunk&) = delete;
+    DataChunk(DataChunk&& other) = delete;
+    DataChunk& operator= (DataChunk&& other) = delete;
     ~DataChunk() = default;
-
-    operator bool() const noexcept { return data != nullptr; }
-    void reset() noexcept { data.reset(); }
-    const char* get() const noexcept { return data.get(); }
 
     std::unique_ptr<char[]> data;
     const std::size_t length;
